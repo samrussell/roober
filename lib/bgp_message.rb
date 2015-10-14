8d6d31cdffd8015744e7963deecfacb993e90864
Sam Russell <sam.h.russell@gmail.com>
2015-10-14 22:32:11 +1300

    two message classes now, more tests

--- lib/bgp_message.rb
+++ lib/bgp_message.rb
@@ -2,19 +2,22 @@ class BGPMessage
   @@subclasses = { }
   # extract word in big endian: substr.unpack('n')[0]
   def self.build_from_packet(raw_packet_data)
-    message_type_number = raw_packet_data.getbyte(18)
-    # TODO should do subclass.build_from_packet() instead
-    @@subclasses[message_type_number].new(raw_packet_data) if is_header_valid?(raw_packet_data)
+    marker, length, message_type = raw_packet_data.unpack('A16S>C')
+    check_header_is_valid(raw_packet_data)
+    @@subclasses[message_type].build_from_packet(raw_packet_data)
   end
 
   private
 
-  def self.is_header_valid?(raw_packet_data)
-    are_first_16_bytes_all_0xff?(raw_packet_data)
+  def self.check_header_is_valid(raw_packet_data)
+    marker, length, message_type = raw_packet_data.unpack('A16S>C')
+    raise ArgumentError, 'Marker is not all 0xFF' unless is_marker_all_0xff?(marker)
   end
 
-  def self.are_first_16_bytes_all_0xff?(raw_packet_data)
-    raw_packet_data[0...16] == "\xff" * 16
+  def self.is_marker_all_0xff?(marker)
+    # TODO: get rid of magic number
+    # TODO: make Marker class?
+    marker.force_encoding('UTF-8') == "\xff" * 16
   end
 
   def self.register_subclass message_type_number
@@ -22,10 +25,26 @@ class BGPMessage
   end
 end
 
+class BGPMessageOpen < BGPMessage
+  MINIMUM_PACKET_LENGTH = 29
+
+  register_subclass 1
+
+  def self.build_from_packet(raw_packet_data)
+    marker, packet_length, message_type, bgp_version, sender_as,
+      hold_time, sender_id, optional_parameters_length,
+      optional_paramters = raw_packet_data.unpack('a16' +
+        'S>' + 'C' + 'C' + 'S>' + 'S>' + 'a4' + 'C' + 'a*')
+    raise ArgumentError, 'packet length is too short' if packet_length < MINIMUM_PACKET_LENGTH
+    raise ArgumentError, 'optional parameters length is off' if optional_parameters_length != packet_length - MINIMUM_PACKET_LENGTH
+    BGPMessageOpen.new
+  end
+end
+
 class BGPMessageKeepalive < BGPMessage
   register_subclass 4
 
-  def initialize(raw_packet_data)
-    @raw_packet_data = raw_packet_data
+  def self.build_from_packet(raw_packet_data)
+    BGPMessageKeepalive.new
   end
 end
--- spec/bgp_message_spec.rb
+++ spec/bgp_message_spec.rb
@@ -3,18 +3,76 @@ require 'bgp_message'
 
 RSpec.describe BGPMessage do
   describe '#build_from_packet' do
+    # packet samples found at http://packetlife.net/captures/protocol/bgp/
+    # https://www.cloudshark.org/captures/004f81c952b7
+    # and at RFC https://www.ietf.org/rfc/rfc4271.txt
     let(:raw_packet_data) { nil }
+    let(:valid_bgp_marker) { ([0xff] * 16).pack("C" * 16).force_encoding('UTF-8') }
+    let(:invalid_bgp_marker) { ([0xff] * 8 + [0x12] + [0xff] * 7).pack("C" * 16).force_encoding('UTF-8') }
     subject(:message) { BGPMessage.build_from_packet(raw_packet_data) }
 
-    context 'with invalid leading data' do
-      # need a cleaner way to mark out these packets
-      let(:raw_packet_data) { "\xFF" * 8 + "\x12" + "\xFF" * 7 + "\x00\x13" + "\x04" }
+    context 'without all 0xFF in the first 16 bytes data' do
+      let(:invalid_keepalive_packet) { invalid_bgp_marker + "\x00\x13" + "\x04" }
+      let(:raw_packet_data) { invalid_keepalive_packet }
       
-      it { is_expected.to be nil }
+      it 'throws an exception' do
+        expect { BGPMessage.build_from_packet(raw_packet_data) }.to raise_error ArgumentError
+      end
+    end
+
+    context 'with a valid open message' do
+      let(:packet_length) { [29].pack('S>').force_encoding('UTF-8') }
+      let(:message_type) { [1].pack('C').force_encoding('UTF-8') }
+      let(:bgp_version) { [4].pack('C').force_encoding('UTF-8') }
+      let(:sender_as) { [30].pack('S>').force_encoding('UTF-8') }
+      let(:hold_time) { [180].pack('S>').force_encoding('UTF-8') }
+      let(:sender_id) { [10, 0, 0, 9].pack('CCCC').force_encoding('UTF-8') }
+      let(:optional_parameters_length) { [0].pack('C').force_encoding('UTF-8') }
+      let(:optional_parameters) { '' }
+      let(:valid_open_packet) { valid_bgp_marker +
+        packet_length +
+        message_type +
+        bgp_version +
+        sender_as +
+        hold_time +
+        sender_id +
+        optional_parameters_length +
+        optional_parameters
+      }
+      let(:raw_packet_data) { valid_open_packet }
+      
+      it { is_expected.to be_a_kind_of BGPMessageOpen }
+    end
+
+    context 'with an invalid open message with bad length' do
+      let(:packet_bad_length) { [28].pack('S>').force_encoding('UTF-8') }
+      let(:message_type) { [1].pack('C').force_encoding('UTF-8') }
+      let(:bgp_version) { [4].pack('C').force_encoding('UTF-8') }
+      let(:sender_as) { [30].pack('S>').force_encoding('UTF-8') }
+      let(:hold_time) { [180].pack('S>').force_encoding('UTF-8') }
+      let(:sender_id) { [10, 0, 0, 9].pack('CCCC').force_encoding('UTF-8') }
+      let(:optional_parameters_length) { [0].pack('C').force_encoding('UTF-8') }
+      let(:optional_parameters) { '' }
+      let(:invalid_open_packet_bad_length) { valid_bgp_marker +
+        packet_bad_length +
+        message_type +
+        bgp_version +
+        sender_as +
+        hold_time +
+        sender_id +
+        optional_parameters_length +
+        optional_parameters
+      }
+      let(:raw_packet_data) { invalid_open_packet_bad_length }
+      
+      it 'throws an exception' do
+        expect { BGPMessage.build_from_packet(raw_packet_data) }.to raise_error ArgumentError
+      end
     end
 
     context 'with a valid keepalive message' do
-      let(:raw_packet_data) { "\xFF" * 16 + "\x00\x13" + "\x04" }
+      let(:valid_keepalive_packet) { valid_bgp_marker + [19, 4].pack('S>C') }
+      let(:raw_packet_data) { valid_keepalive_packet }
       
       it { is_expected.to be_a_kind_of BGPMessageKeepalive }
     end
