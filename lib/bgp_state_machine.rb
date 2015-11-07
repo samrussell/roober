7fb3e9bd3173edae749b2ca825fdaf157a7bce1f
Sam Russell <sam.h.russell@gmail.com>
2015-11-08 11:55:50 +1300

    started putting in code, also fixed guardfile

--- lib/bgp_state_machine.rb
+++ lib/bgp_state_machine.rb
@@ -1,2 +1,13 @@
 class BGPStateMachine
+  attr_reader :state
+
+  def initialize
+    @state = :idle
+  end
+
+  def event(event_name)
+    if @state == :idle && event_name == :manual_start
+      @state = :connect
+    end
+  end
 end
--- spec/bgp_state_machine_spec.rb
+++ spec/bgp_state_machine_spec.rb
@@ -8,7 +8,24 @@ describe BGPStateMachine do
     expect(bgp_state_machine.state).to eq(:idle)
   end
 
-  context 'when it receives a valid Open message' do
-    it 'transitions to the Active state'
+  context 'when in the idle state' do
+    context 'when it receives a ManualStart event' do
+      it 'transitions to the connect state' do
+        expect { bgp_state_machine.event(:manual_start) }.to change { bgp_state_machine.state }.from(:idle).to(:connect)
+      end
+    end
+    
+    context 'when it receives a ManualStop event' do
+    end
+  end
+
+  context 'when in the connect state' do
+    context 'when it receives a valid Open message' do
+      it 'transitions to the Active state'
+    end
+
+    context 'when it receives an invalid Open message' do
+      it 'sends a notification message and closes the connection'
+    end
   end
 end
