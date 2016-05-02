affd77fd461a02c8dc6d1d342dbd9161e3336ae7
Sam Russell <sam.h.russell@gmail.com>
2016-05-02 21:35:45 +1200

    multicast listen

--- ldpd.rb
+++ ldpd.rb
@@ -1,13 +1,16 @@
 #!/bin/env ruby
 
 require 'socket'
+require 'ipaddr'
 require './lib/io_slicer'
 require './lib/mailbox'
 #require './lib/bgp_state_machine'
 #require './lib/bgp_message'
 
 socket = UDPSocket.new
-socket.bind('192.168.99.2', 646)
+ip_mreq = IPAddr.new("224.0.0.2").hton + IPAddr.new("10.10.10.1").hton
+socket.setsockopt(Socket::IPPROTO_IP, Socket::IP_ADD_MEMBERSHIP, ip_mreq)
+socket.bind(Socket::INADDR_ANY, 646)
 
 loop do
   mesg, sender_info = socket.recvfrom(1500)
