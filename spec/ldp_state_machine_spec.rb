f5b2575d193ed771c37c7907d18e3aa0bd008e99
Sam Russell <sam.h.russell@gmail.com>
2016-05-15 14:15:43 +1200

    LDP state machine passive role tests done

--- spec/ldp_state_machine_spec.rb
+++ spec/ldp_state_machine_spec.rb
@@ -6,49 +6,54 @@ describe LDPStateMachine do
   let(:mailbox) { instance_double('Mailbox') }
   subject(:ldp_state_machine) { LDPStateMachine.new(mailbox) }
 
-  it 'starts in the idle state' do
-    expect(ldp_state_machine.state).to eq(:idle)
+  it 'starts in the non_existent state' do
+    expect(ldp_state_machine.state).to eq(:non_existent)
   end
 
-  context 'when in the idle state' do
-    context 'when it receives a ManualStartPassive event' do
+  context 'when in the non_existent state' do
+    context 'when it receives a TCP connection confirmed event' do
       context 'passive' do
-        it 'transitions to the active state' do
-          expect { ldp_state_machine.event(:manual_start_passive) }.to change { ldp_state_machine.state }.from(:idle).to(:active)
+        it 'transitions to the initialised state' do
+          expect { ldp_state_machine.event(:tcp_connection_confirmed) }.to change { ldp_state_machine.state }.from(:non_existent).to(:initialised)
         end
       end
     end
   end
 
-  context 'when in the Active state' do
+  context 'when in the Initialised state' do
     before do
-      ldp_state_machine.event(:manual_start_passive)
+      ldp_state_machine.event(:tcp_connection_confirmed)
     end
 
-    context 'when it receives a TcpConnectionConfirmed event' do
-      it 'doesn\'t change state' do
-        expect(ldp_state_machine.state).to eq(:active)
+    context 'when it receives an Initialise event' do
+      context 'passive' do
+        it 'sends an initialisation message followed by a keepalive message' do
+          expect(:mailbox).to receive(:send_message).with a_kind_of(LDPMessageInitialise).ordered
+          expect(:mailbox).to receive(:send_message).with a_kind_of(LDPMessageKeepalive).ordered
+        end
 
-        expect { ldp_state_machine.event(:tcp_connection_confirmed) }.to_not change { ldp_state_machine.state }
+        it 'transitions to the openrec state' do
+          expect(ldp_state_machine.state).to eq(:initialised)
+
+          expect { ldp_state_machine.event(:initialise) }.to change { ldp_state_machine.state }.from(:initialised).to(:openrec)
+        end
       end
     end
+  end
 
-    xcontext 'when it receivs a valid Open message' do
-      let(:mock_ldp_open_message) { instance_double(LDPMessageOpen) }
+  context 'when in the Openrec state' do
+    before do
+      ldp_state_machine.event(:tcp_connection_confirmed)
+      ldp_state_machine.event(:initialise)
+    end
 
-      before do
-        allow(mock_ldp_open_message).to receive(:message_type).and_return(LDPMessageOpen::MESSAGE_CODE)
-      end
+    context 'when it receives a Keepalive event' do
+      context 'passive' do
+        it 'transitions to the Operational state' do
+          expect(ldp_state_machine.state).to eq(:openrec)
 
-      it 'sends an open message, a keepalive message, and changes state to OpenConfirm' do
-        expect(ldp_state_machine.state).to eq(:active)
-        
-        expect(mailbox).to receive(:send_message).with a_kind_of(LDPMessageOpen)
-        expect(mailbox).to receive(:send_message).with a_kind_of(LDPMessageKeepalive)
-
-        ldp_state_machine.message(mock_ldp_open_message)
-
-        expect(ldp_state_machine.state).to eq(:open_confirm)
+          expect { ldp_state_machine.event(:keepalive) }.to change { ldp_state_machine.state }.from(:openrec).to(:operational)
+        end
       end
     end
   end
