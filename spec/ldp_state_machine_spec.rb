f8dec7778619c4302a166ac39267d3ae592076c7
Sam Russell <sam.h.russell@gmail.com>
2016-08-26 21:38:48 +1200

    reorder ldp spec

--- spec/ldp_state_machine_spec.rb
+++ spec/ldp_state_machine_spec.rb
@@ -4,6 +4,8 @@ require './lib/ldp_state_machine'
 require './lib/mailbox'
 
 describe LDPStateMachine do
+  let(:mock_ldp_keepalive_message) { instance_double(LDPMessageKeepalive, :message_type => LDPMessageKeepalive::MESSAGE_CODE) }
+  let(:mock_ldp_initialise_message) { instance_double(LDPMessageInitialization, :message_type => LDPMessageInitialization::MESSAGE_CODE) }
   let(:mailbox) { instance_double('Mailbox') }
   subject(:ldp_state_machine) { LDPStateMachine.new(mailbox) }
 
@@ -11,29 +13,21 @@ describe LDPStateMachine do
     expect(ldp_state_machine.state).to eq(:non_existent)
   end
 
-  context 'when in the non_existent state' do
-    context 'when it receives a TCP connection confirmed event' do
-      context 'passive' do
+  context 'passive mode' do
+    context 'when in the non_existent state' do
+      context 'when it receives a TCP connection confirmed event' do
         it 'transitions to the initialised state' do
           expect { ldp_state_machine.event(:tcp_connection_confirmed) }.to change { ldp_state_machine.state }.from(:non_existent).to(:initialised)
         end
       end
     end
-  end
 
-  context 'when in the initialised state' do
-    before do
-      ldp_state_machine.event(:tcp_connection_confirmed)
-    end
-
-    context 'when it receives an Initialization message' do
-      let(:mock_ldp_initialise_message) { instance_double(LDPMessageInitialization) }
-
+    context 'when in the initialised state' do
       before do
-        allow(mock_ldp_initialise_message).to receive(:message_type).and_return(LDPMessageInitialization::MESSAGE_CODE)
+        ldp_state_machine.event(:tcp_connection_confirmed)
       end
 
-      context 'passive' do
+      context 'when it receives an Initialization message' do
         it 'sends an initialisation message followed by a keepalive message and transitions to the openrec state' do
           expect(mailbox).to receive(:send_message).ordered.with(a_kind_of(LDPMessageInitialization))
           expect(mailbox).to receive(:send_message).ordered.with(a_kind_of(LDPMessageKeepalive))
@@ -45,26 +39,17 @@ describe LDPStateMachine do
         end
       end
     end
-  end
 
-  context 'when in the Openrec state' do
-    let(:mock_ldp_initialise_message) { instance_double(LDPMessageInitialization) }
-    let(:mock_ldp_keepalive_message) { instance_double(LDPMessageKeepalive) }
+    context 'when in the Openrec state' do
+      before do
+        allow(mailbox).to receive(:send_message).ordered.with(a_kind_of(LDPMessageInitialization))
+        allow(mailbox).to receive(:send_message).ordered.with(a_kind_of(LDPMessageKeepalive))
 
-    before do
-      # TODO set attr_readers in let block
-      allow(mock_ldp_keepalive_message).to receive(:message_type).and_return(LDPMessageKeepalive::MESSAGE_CODE)
-      allow(mock_ldp_initialise_message).to receive(:message_type).and_return(LDPMessageInitialization::MESSAGE_CODE)
+        ldp_state_machine.event(:tcp_connection_confirmed)
+        ldp_state_machine.message(mock_ldp_initialise_message)
+      end
 
-      allow(mailbox).to receive(:send_message).ordered.with(a_kind_of(LDPMessageInitialization))
-      allow(mailbox).to receive(:send_message).ordered.with(a_kind_of(LDPMessageKeepalive))
-
-      ldp_state_machine.event(:tcp_connection_confirmed)
-      ldp_state_machine.message(mock_ldp_initialise_message)
-    end
-
-    context 'when it receives a Keepalive message' do
-      context 'passive' do
+      context 'when it receives a Keepalive message' do
         it 'transitions to the Operational state' do
           expect(ldp_state_machine.state).to eq(:openrec)
 
