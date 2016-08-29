845c38798567d265946120cce408ec385717277c
Sam Russell <sam.h.russell@gmail.com>
2016-08-29 21:43:49 +1200

    build common session parameter

--- lib/ldp_parameter.rb
+++ lib/ldp_parameter.rb
@@ -78,3 +78,55 @@ class LDPParameterIPv4Address < LDPParameter
     new(unpacked_data.ip_address_packed)
   end
 end
+
+class LDPParameterCommonSession < LDPParameter
+  class UnpackedLDPParameterCommonSession < Struct.new(:code, :length, :protocol_version, :keepalive_time, :flags, :path_vector_limit, :max_pdu_length, :lsr_id, :label_space_id)
+  end
+
+  PARAMETER_CODE = 0x500
+  DEFAULT_LENGTH = 14
+  UNPACK_STRING = 'S>S>S>S>CCS>L>S>'
+  PACK_STRING = 'S>S>S>S>CCS>L>S>'
+
+  register_subclass PARAMETER_CODE
+
+  attr_reader :protocol_version, :keepalive_time, :flags, :path_vector_limit, :max_pdu_length, :lsr_id, :label_space_id
+
+  def initialize(protocol_version, keepalive_time, flags, path_vector_limit, max_pdu_length, lsr_id, label_space_id)
+    @protocol_version = protocol_version
+    @keepalive_time = keepalive_time
+    @flags = flags
+    @path_vector_limit = path_vector_limit
+    @max_pdu_length = max_pdu_length
+    @lsr_id = IPAddr.new(lsr_id, Socket::PF_INET)
+    @label_space_id = label_space_id
+  end
+
+  def pack
+    [
+      PARAMETER_CODE,
+      DEFAULT_LENGTH,
+      protocol_version,
+      keepalive_time,
+      flags,
+      path_vector_limit,
+      max_pdu_length,
+      lsr_id.to_i,
+      label_space_id
+    ].pack(PACK_STRING)
+  end
+
+  def self.build_from_packet(raw_packet_data)
+    unpacked_data = UnpackedLDPParameterCommonSession.new(*raw_packet_data.unpack(UNPACK_STRING))
+
+    new(unpacked_data.protocol_version,
+        unpacked_data.keepalive_time,
+        unpacked_data.flags,
+        unpacked_data.path_vector_limit,
+        unpacked_data.max_pdu_length,
+        unpacked_data.lsr_id,
+        unpacked_data.label_space_id
+       )
+  end
+end
+
--- spec/ldp_parameter_spec.rb
+++ spec/ldp_parameter_spec.rb
@@ -27,3 +27,45 @@ RSpec.describe LDPParameterIPv4Address do
   end
 end
 
+RSpec.describe LDPParameterCommonSession do
+  let(:code) { [0x0500].pack("S>") }
+  let(:length) { [14].pack("S>") }
+  let(:protocol_version) { [1].pack("S>") }
+  let(:keepalive_time) { [15].pack("S>") }
+  let(:flags) { [0].pack("C") }
+  let(:path_vector_limit) { [0].pack("C") }
+  let(:max_pdu_length) { [0].pack("S>") }
+  let(:lsr_id) { [10, 1, 1, 1].pack("CCCC") }
+  let(:label_space_id) { [0].pack("S>") }
+  let(:packed_parameter) {  code +
+                            length +
+                            protocol_version +
+                            keepalive_time +
+                            flags +
+                            path_vector_limit +
+                            max_pdu_length +
+                            lsr_id +
+                            label_space_id
+                          }
+  let(:parameter) { LDPParameter.build_from_packet(packed_parameter) }
+  let(:repacked_parameter) { parameter.pack }
+
+  describe '.build_from_packet' do
+    it 'unpacks the parameter' do
+      expect(parameter.protocol_version).to eq(1)
+      expect(parameter.keepalive_time).to eq(15)
+      expect(parameter.flags).to eq(0) # TODO test actual booleans
+      expect(parameter.path_vector_limit).to eq(0)
+      expect(parameter.max_pdu_length).to eq(0)
+      expect(parameter.lsr_id.to_s).to eq("10.1.1.1")
+      expect(parameter.label_space_id).to eq(0)
+    end
+  end
+
+  describe '#pack' do
+    it 'packs the parameter' do
+      expect(repacked_parameter).to eq(packed_parameter)
+    end
+  end
+end
+
